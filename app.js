8eed1cdad2618d5e7126fd6a795cef33a2cdfa53
unknown <nkarnik@NA01805D.na.corp.intven.com>
2015-02-12 00:33:41 -0800

    app

--- app.js
+++ app.js
@@ -1,4 +1,18 @@
 var express = require('express');
+var app = express();
+
+app.set('port', (process.env.PORT || 5000));
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function(request, response) {
+  response.send('Hello World!');
+});
+
+app.listen(app.get('port'), function() {
+  console.log("Node app is running at localhost:" + app.get('port'));
+});
+
+/*var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
@@ -58,3 +72,4 @@ app.use(function(err, req, res, next) {
 
 
 module.exports = app;
+*/
