787bcb2d44473cfa336306a8e22c396fb656a0af
Jason Cochran <me@jasonleecochran.com>
2015-02-27 16:32:21 -0600

    store menu, icons (svg and png), css adjustments overall

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var routes = require('./routes/routes.js');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 var bike = require('./routes/bike');
+var router = express.Router();
 
 //var dbName = 'bikeDB';
 //var devUrl = 'mongodb://localhost:27017/'
@@ -17,7 +18,42 @@ app.set('view engine', 'jade');
 app.set('port', (process.env.PORT || 4000));
 app.use(express.static(__dirname + '/public'));
 
+// array of menu items
+// these relate directly to the menu icon file names
+// TODO: feed this with http.get call
+var menu_items = [
+  {_id: 12345, name: "accessories"},
+  {_id: 12345, name: "allparts"},
+  {_id: 12345, name: "bottombrackets"},
+  {_id: 12345, name: "brakelevers"},
+  {_id: 12345, name: "brakerotors"},
+  {_id: 12345, name: "chainrings"},
+  {_id: 12345, name: "cogs"},
+  {_id: 12345, name: "crankset"},
+  {_id: 12345, name: "forksdouble"},
+  {_id: 12345, name: "forksrigid"},
+  {_id: 12345, name: "forkssingle"},
+  {_id: 12345, name: "grips"},
+  {_id: 12345, name: "handlebars"},
+  {_id: 12345, name: "headsets"},
+  {_id: 12345, name: "hubs"},
+  {_id: 12345, name: "pedals"},
+  {_id: 12345, name: "rearshockair"},
+  {_id: 12345, name: "rearshockcoil"},
+  {_id: 12345, name: "rims"},
+  {_id: 12345, name: "seatposts"},
+  {_id: 12345, name: "shifter"},
+  {_id: 12345, name: "spokes"},
+  {_id: 12345, name: "stems"},
+  {_id: 12345, name: "tires"}
+]
 
+// pass in menu_items array so entire app has access
+// to it
+app.use(function(req, res, next) {
+  res.locals.menu_items = menu_items;
+  next();
+});
 
 /////////////////////////////////////////
 // client page routes
@@ -78,8 +114,8 @@ router.get('/mybike/frame/', function(req, res) {
 });
 
 
-var herokuURL = process.env.MONGOLAB_URI;
-mongoose.connect(herokuURL);
+// var herokuURL = process.env.MONGOLAB_URI;
+// mongoose.connect(herokuURL);
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
@@ -113,16 +149,12 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
 var routes = require('./routes/index');
 var users = require('./routes/users');
-
 var app = express();
-
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
-
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
@@ -130,19 +162,15 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
 app.use('/', routes);
 app.use('/users', users);
-
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
     var err = new Error('Not Found');
     err.status = 404;
     next(err);
 });
-
 // error handlers
-
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
@@ -154,7 +182,6 @@ if (app.get('env') === 'development') {
         });
     });
 }
-
 // production error handler
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
@@ -164,7 +191,5 @@ app.use(function(err, req, res, next) {
         error: {}
     });
 });
-
-
 module.exports = app;
 */
--- public/javascripts/scripts.js
+++ public/javascripts/scripts.js
@@ -1,18 +1,16 @@
 $( document ).ready(function() {
-	// $("#btn_store").click(function(e){
-	// 	e.preventDefault();
-	// 	$("div#storemenu").toggle();
-	// });
-	
 	// show hide store menu
 	$("#btn_store")
 		.mouseenter(function(e){
 			$("div#storemenu").show();
 		})
-	$("#div#storemenu")
-		.mouseleave(function(e){
-			$("div#storemenu").hide();
-		})
+		.click(function(e){
+			$("div#storemenu").toggle();
+		});
+	// $("div#storemenu")
+	// 	.mouseleave(function(e){
+	// 		$("div#storemenu").hide();
+	// 	});
 
 	// menu hover
 	$("div#storemenu table tr td a img")
@@ -28,4 +26,5 @@ $( document ).ready(function() {
 				return attr.replace("on", "off");
 			});
 		});
+
 });
